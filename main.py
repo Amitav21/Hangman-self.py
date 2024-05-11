c6df8bd675c4b39b9066207a7b4aa451e47abaf9
Amitav21 <avivamit4u@gmail.com>
2024-05-11 18:40:17 +0300

    added a function that prints the 7 possible states of the game

--- main.py
+++ main.py
@@ -13,5 +13,50 @@ def openingScreen():
         amount_of_lives = random.randint(5,10)
         print(amount_of_lives)
 
+def printStates():
+    print("first state:")
+    print("    x-------x")
+    print("second state:")
+    print("""    x-------x
+    |
+    |
+    |
+    |
+    |""")
+    print("third state:")
+    print("""    x-------x
+    |       |
+    |       0
+    |
+    |
+    |""")
+    print("fourth state:")
+    print("""    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |""")
+    print("fifth state:")
+    print("""    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |
+    |""")
+    print("sixth state:")
+    print("""    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      /
+    |""")
+    print("seventh state:")
+    print("""    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      / \\
+    |""")
 if __name__ == '__main__':
     openingScreen()
