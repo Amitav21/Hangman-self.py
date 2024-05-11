d713f3a39a8053638136036e811ef1e29ccc0bce
Amitav21 <avivamit4u@gmail.com>
2024-05-11 20:29:41 +0300

    added a winner check function and improved the code

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import random
 
-def openingScreen():
-    HANGMAN_ASCII_ART = """
+HANGMAN_PHOTOS = {1: "    x-------x", 2: """    x-------x
+    |
+    |
+    |
+    |
+    |""", 3: """    x-------x
+    |       |
+    |       0
+    |
+    |
+    |""", 4: """    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |""", 5: """    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |
+    |""", 6: """    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      /
+    |""", 7: """    x-------x
+    |       |
+    |       0
+    |      /|\\
+    |      / \\
+    |"""}
+
+HANGMAN_ASCII_ART = """
 Welcome to the game Hangman
       _    _                                         
      | |  | |                                        
@@ -11,71 +42,50 @@ Welcome to the game Hangman
      |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                           __/ |                      
                          |___/"""
-    MAX_TRIES = 6
+MAX_TRIES = 6
+
+def opening_screen():
     print(HANGMAN_ASCII_ART,"\n",MAX_TRIES)
 
-def guessAletter():
-    chosen_letter = input("please guess a character: ")
+def guess_a_letter():
+    chosen_letter = input("guess a character: ")
     chosen_letter = chosen_letter.lower()
-    if len(chosen_letter) > 1 and not chosen_letter.isalpha():
-        print("E3")
-    elif len(chosen_letter) > 1:
-        print("E1")
-    elif not chosen_letter.isalpha():
-        print("E2")
-    else:
-        print(chosen_letter)
+    print(is_valid_letter(chosen_letter))
 
-def showBoard():
+def is_valid_letter(guessed_letter, old_letters_list):
+    if len(guessed_letter) > 1 or not guessed_letter.isalpha() or guessed_letter in old_letters_list:
+        return False
+    return True
+
+def try_update_letter_guessed(letter_guessed, old_letters_guessed):
+    if is_valid_letter(letter_guessed,old_letters_guessed):
+        old_letters_guessed.append(letter_guessed)
+
+def show_hidden_word(secret_word, old_letters_guessed):
+    result = ""
+    for word in secret_word:
+        if word in old_letters_guessed:
+            result += word + " "
+        else:
+            result += '_ '
+    return result
+
+def check_win(secret_word, old_letters_guessed):
+    for word in secret_word:
+        if word not in old_letters_guessed:
+            return False
+    return True
+
+def show_board():
     chosen_word = input("please enter a word: ")
     word_count = len(chosen_word)
     print("_ " * word_count)
 
-def printStates():
-    print("first state:")
-    print("    x-------x")
-    print("second state:")
-    print("""    x-------x
-    |
-    |
-    |
-    |
-    |""")
-    print("third state:")
-    print("""    x-------x
-    |       |
-    |       0
-    |
-    |
-    |""")
-    print("fourth state:")
-    print("""    x-------x
-    |       |
-    |       0
-    |       |
-    |
-    |""")
-    print("fifth state:")
-    print("""    x-------x
-    |       |
-    |       0
-    |      /|\\
-    |
-    |""")
-    print("sixth state:")
-    print("""    x-------x
-    |       |
-    |       0
-    |      /|\\
-    |      /
-    |""")
-    print("seventh state:")
-    print("""    x-------x
-    |       |
-    |       0
-    |      /|\\
-    |      / \\
-    |""")
+def print_hangman(num_of_tries):
+    print(HANGMAN_PHOTOS[state_index])
+
 if __name__ == '__main__':
-    openingScreen()
-    guessAletter()
+    secret_word = "yes"
+    old_letters_guessed = old_letters_guessed = ['d', 'g', 'e', 'i', 'k', 'y']
+    print(show_hidden_word(secret_word, old_letters_guessed))
+    print(check_win(secret_word,old_letters_guessed))
